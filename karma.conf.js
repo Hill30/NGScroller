c72b8a9f8756785a2a8754f672b681b1e72af0c6
Michael Feingold <mfeingold@hill30.com>
2013-08-10 19:59:54 -0500

    upgraded to karma 0.10

--- karma.conf.js
+++ karma.conf.js
@@ -1,77 +1,75 @@
 // Karma configuration
-// Generated on Fri Jun 21 2013 16:07:05 GMT-0500 (Central Daylight Time)
+// Generated on Sat Aug 10 2013 19:47:03 GMT-0500 (Central Daylight Time)
+
+module.exports = function(config) {
+    config.set({
+
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+        // frameworks to use
+        frameworks: ['jasmine'],
 
 
-frameworks=[
-  'jasmine'
-];
-
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'http://code.jquery.com/jquery-1.9.1.js',
-  'http://code.angularjs.org/1.0.6/angular.js',
-  'http://code.angularjs.org/1.0.6/angular-mocks.js',
-  'public/scripts/**/*.js',
-  'test/**/*Spec.js'
-];
+        // list of files / patterns to load in the browser
+        files: [
+            'http://code.jquery.com/jquery-1.9.1.js',
+            'http://code.angularjs.org/1.0.6/angular.js',
+            'http://code.angularjs.org/1.0.6/angular-mocks.js',
+            'public/scripts/**/*.js',
+            'test/**/*Spec.js'
+        ],
 
 
-// list of files to exclude
-exclude = [
-  
-];
+        // list of files to exclude
+        exclude: [
+
+        ],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['progress'],
 
 
-// web server port
-port = 9876;
+        // web server port
+        port: 9876,
 
 
-// cli runner port
-runnerPort = 9100;
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = false;
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['Chrome','IE'],//, 'Firefox'],
+        //browsers = ['Firefox'];
+        //browsers = ['Chrome'];
+        //browsers = ['IE'];
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome','IE', 'Firefox'];
-//browsers = ['Firefox'];
-//browsers = ['Chrome'];
-//browsers = ['IE'];
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
+};
